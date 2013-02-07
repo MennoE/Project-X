4a53189b73716edcc08d5f6fd931c9e91086bb30
Menno Egbers <menno.egbers@kega.nl>
2013-02-07 15:53:34 +0100

    [PX] +: create new public function getStoreCategoryId()
    This functinon retries the required store id from the Project-X config

--- app/code/local/Kega/Init/Helper/Data.php
+++ app/code/local/Kega/Init/Helper/Data.php
@@ -7,4 +7,9 @@
 class Kega_Init_Helper_Data extends Mage_Core_Helper_Abstract
 {
     const XML_STORE_CATEGORY_ID = 'extrasettings/store_settings/store_category_id';
+
+    public function getStoreCategoryId()
+    {
+        return Mage::getStoreConfig(self::XML_STORE_CATEGORY_ID);
+    }
 }
