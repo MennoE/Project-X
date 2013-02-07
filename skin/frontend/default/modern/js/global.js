373c1b33ef42b5602399aea5021b48d6340e43a0
Menno Egbers <menno.egbers@kega.nl>
2013-02-07 15:56:50 +0100

    [PX] +: optimize listpage template & improve js script
    I was able to use the existing classnames in the store_list template. Therefor the wrapper classname are no longer necessary. This also enhances the flexibility for implementation on other pages or projects.
    I also found some flaws in the js script causing the interactions to malfunction. I was able to fix a few things but not everything yet.
    Missing part is collaps the expanded tab when other tab is clicked.

--- skin/frontend/default/modern/js/global.js
+++ skin/frontend/default/modern/js/global.js
@@ -15,9 +15,8 @@ var Initialize = function($) {
 	};
 
 	// collect available tab sizes in variables
-	var wrapperWidth;
-	var itemSize;
-    var itemWidths;
+	var newWidth;
+    var singleWidth;
 
 	/*
 	* collect the widths of the tabs and total width of the tab wrapper
@@ -28,24 +27,32 @@ var Initialize = function($) {
 		var itemWidths = 0;
 
 		// get the width of wrapper
-		var wrapperWidth = $('.wrapper').width();
+		var wrapperWidth = $('.category-products').width();
 
-		// get the width for each tab
-		$('.tab-wrapper li').each(function(){
-			itemWidths = $(this).width();
+        // get width of one single tab
+        var singleWidth = $('.products-grid .item').width();
+
+		// get the width for all tabs together
+		$('.products-grid .item').each(function(){
+			itemWidths += $(this).width()
 		});
 
 		// to calculate the remaining width available for the selected, clicked tab
-		var itemSize = wrapperWidth - itemWidths;
+		var combinedWidth = wrapperWidth - itemWidths;
 
-		// make the hovered tab recognisable by using an 'active' class which can be used to styling and interaction purposes
-		$(".wrapper").delegate('li', 'hover', function(){
-			$('li').removeClass('active');
+        // since we have to set a new width to the clicked tab we need to
+        // add the remaining width to the width of one single tab
+        var newWidth = singleWidth + combinedWidth;
+
+		// make the hovered tab recognisable by using an 'active' class
+		// which can be used to styling and interaction purposes
+		$(".category-products").delegate('.item', 'hover', function(){
+			$('.item').removeClass('active');
 			$(this).addClass('active');
 		})
 
-		return wrapperWidth,
-			   itemSize;
+		return singleWidth,
+               newWidth;
 	};
 
 	/*
@@ -53,18 +60,22 @@ var Initialize = function($) {
 	 **/
 	var clickTab = function()
 	{
-		$(".tab-wrapper").delegate('li', 'click', function(){
-            $('li').animate({
-                width: resize(itemSize)+'px'
-            }, 1500, function(){
-                $(this).removeClass('expand');
-            });
+		$(".products-grid").delegate('.item', 'click', function(){
+            if($('.item').hasClass('expand')){
+                $(this).animate({
+                    width: resize(singleWidth)+'px'
+                }, 1500, function(){
+                    $('.item').removeClass('expand');
+                });
+            };
 
-			$(this).animate({
-				width: resize(itemSize)+'px'
-			}, 1500, function(){
-				$(this).addClass('expand');
-			});
+            if(!$('.item').hasClass('expand')){
+                $(this).animate({
+                    width: resize(newWidth)+'px'
+                }, 1500, function(){
+                    $(this).addClass('expand');
+                });
+            };
 		});
 	};
 
